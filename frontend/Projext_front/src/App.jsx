e8773254c2baad065d5eec3061d83c1dcea63554
Eduardoh5179 <eduardorsim7@gmail.com>
2025-11-13 15:21:45 -0300

    instalando dependencias

---
+++ frontend/Projext_front/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function Header() {
+  return(
+    <div>
+      <a href="">Início</a>
+      <a href=""> Notícias </a>
+      <a href=""> Projetos</a>
+      <a href="">Contato</a>
+      <a href="">Inscrever-se</a>
+    </div>
+  )
+}
+
+export default Header
