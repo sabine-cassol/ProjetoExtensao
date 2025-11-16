950af1038d928d09220c3dd68cbe039ca74d8bfd
Eduardoh5179 <eduardorsim7@gmail.com>
2025-11-16 01:41:53 -0300

    feat: criando rotas para paginas com o react router

--- frontend/Projext_front/src/main.jsx
+++ frontend/Projext_front/src/main.jsx
@@ -1,13 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HeaderComponent from './Header.jsx'
 import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from "./pages/Login.jsx"
+import Noticias from "./pages/Noticias.jsx"
+import Home from './pages/Home.jsx'
+import Error from './pages/Error.jsx'
+
+const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Home/>,
+      errorElement: <Error/>
+    },
+    {
+      path:"/Login",
+      element: <Login/>
+    },
+    {
+      path:"/Noticias",
+      element: <Noticias/>
+    }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <HeaderComponent />
+      <RouterProvider router={router}/>
     </>
   </StrictMode>,
 )
