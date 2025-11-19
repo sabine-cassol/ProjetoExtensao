7481c63aa961046ae7d5d249801d6c59b3555135
Eduardoh5179 <eduardorsim7@gmail.com>
2025-11-19 15:59:57 -0300

    feat:Area de notícias

---
+++ frontend/Projext_front/src/components/NewsGrid.jsx
@@ -0,0 +1,70 @@
+function NewsGrid(){
+    return (
+    
+            <section className="newsGridContainer">
+                <section className="news">
+                    <div className="new">
+                        <div className="newImage">
+                        
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet
+                        </h3>
+ 
+                    </div>
+                    <div className="new">
+                        <div className="newImage">
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet 
+                        </h3>
+                    </div>
+                    <div className="new">
+                        <div className="newImage">                        
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet
+                        </h3>
+                    </div>
+                        <div className="new">
+                        <div className="newImage">                        
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet 
+                        </h3>
+                    </div>
+                        <div className="new">
+                        <div className="newImage">                        
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet
+                        </h3>
+                    </div>
+                        <div className="new">
+                        <div className="newImage">                        
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet
+                        </h3>
+                    </div>
+                        <div className="new">
+                        <div className="newImage">                        
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet
+                        </h3>
+                    </div>
+                        <div className="new">
+                        <div className="newImage">                        
+                        </div>
+                        <h3 className="newTitle">
+                        Lorem ipsum dolor sit amet
+                        </h3>
+                    </div>
+                </section>
+            </section>
+        
+    )
+}
+
+export default NewsGrid
--- frontend/Projext_front/src/pages/Noticias.jsx
+++ frontend/Projext_front/src/pages/Noticias.jsx
@@ -1,5 +1,7 @@
 import Header from  '../components/Header'
 import SideBar1 from '../components/SideBar1'
+import NewsGrid from '../components/NewsGrid'
+import '../Styles/News.css'
 
 
 import { useState } from "react"
@@ -7,12 +9,16 @@ import { useState } from "react"
 export default function Noticias(){
 
     const [showSideBar, setShowSideBar] = useState(true);
+    const mainContentClass = showSideBar ? 'newsShifted' : 'main-content';
     return (
         <>
         <Header aoClick={()=>{
             setShowSideBar(!showSideBar)
         }} />
         <SideBar1 visivel={showSideBar}/>
+        <main className={mainContentClass}> 
+                <NewsGrid />
+        </main>
         </>
     )
 }
