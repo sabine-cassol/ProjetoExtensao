17608cc1d6c19265539d1eb7e1a09bab293c1da2
Eduardoh5179 <eduardorsim7@gmail.com>
2026-06-15 16:50:59 -0300

    feat: adicionando hierarquia visual diferenta para papéis diferentes no sistema

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,12 @@ import Logo2 from '../assets/IMG_20251114_003344.png'
 import { Menu } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import { useSidebar } from "@/components/ui/sidebar"
+import { useAuth } from '../context/AuthContext';
 
 function Header() {
 
-    const { toggleSidebar } = useSidebar()
+    const { toggleSidebar } = useSidebar();
+    const { role } = useAuth();
 
     return (
         <>
@@ -15,10 +17,13 @@ function Header() {
                     <div className="flex justify-between items-center h-16">
 
                         <div className="flex flex-row justify-self-start items-center gap-3">
-                            <button onClick={toggleSidebar} className="flex items-center justify-center h-9 w-9 rounded-full cursor-pointer transition-colors duration-200 hover:bg-zinc-500/10 active:bg-zinc-500/20 focus:outline-hidden">
-                                <Menu className="h-5 w-5 text-foreground" />
-                            </button>
 
+
+                            {role === 'student' || role === 'teacher' ? (
+                                <button onClick={toggleSidebar} className="flex items-center justify-center h-9 w-9 rounded-full cursor-pointer transition-colors duration-200 hover:bg-zinc-500/10 active:bg-zinc-500/20 focus:outline-hidden">
+                                    <Menu className="h-5 w-5 text-foreground" />
+                                </button>
+                            ) : null}
 
                             <picture>
                                 <source srcSet={Logo} media="(min-width: 860px)" />
@@ -40,14 +45,14 @@ function Header() {
                                 Contato
                             </Link>
                         </nav>
-
                         <div className="col-start-3 flex justify-end items-center gap-2 relative">
-
-                            <Link to="Login">
-                                <button className='p-2 px-4 cursor-pointer  text-white font-semibold rounded-lg bg-cyan-400 hover:bg-cyan-500 active:bg-cyan-400'>
-                                    Inscrever-se
-                                </button>
-                            </Link>
+                            {role === 'guest' && (
+                                <Link to="Login">
+                                    <button className='p-2 px-4 cursor-pointer  text-white font-semibold rounded-lg bg-cyan-400 hover:bg-cyan-500 active:bg-cyan-400'>
+                                        Inscrever-se
+                                    </button>
+                                </Link>
+                            )}
                         </div>
                     </div>
                 </div>
