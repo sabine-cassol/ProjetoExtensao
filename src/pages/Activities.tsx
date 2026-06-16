229da43ebf515799c22db9d3e183b2a370228027
Eduardoh5179 <eduardorsim7@gmail.com>
2026-06-16 19:00:39 -0300

    feat: adicionando dados hardcoded para páginas de relatório e atividades

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -3,8 +3,8 @@ import { SidebarProvider } from "../components/ui/sidebar.tsx"
 import { AppSidebar } from "../components/app-sidebar.tsx"
 import Footer from "../components/Footer.tsx"
 
-function Activities(){
-    return(
+function Activities() {
+    return (
         <>
             <SidebarProvider>
                 <div className="flex flex-col w-screen min-h-screen">
@@ -12,13 +12,42 @@ function Activities(){
                     <div className="flex flex-1 w-full">
                         <AppSidebar />
 
-                        <main className="p-4 overflow-y-auto ">
-                            <h1 className="text-4xl font-bold overflow-hidden">Atividades</h1>
+                        <main className="p-4 flex-1 ">
+                            <h1 className="text-4xl font-bold overflow-hidden ">Atividades</h1>
+                            <section className='mt-4'>
+
+                                <table className="min-w-full divide-y border border-zinc-200 divide-gray-200 bg-white dark:bg-zinc-900 text-sm">
+                                    <thead className="bg-gray-50 dark:bg-zinc-800/50">
+                                        <tr>
+                                            <th className="px-4 py-3 text-left font-semibold text-gray-900 dark:text-zinc-200">Nome</th>
+                                            <th className="px-4 py-3 text-left font-semibold text-gray-900 dark:text-zinc-200">data de Inicio</th>
+                                            <th className="px-4 py-3 text-left font-semibold text-gray-900 dark:text-zinc-200">data de encerramento</th>
+                                            <th className="px-4 py-3 text-right font-semibold text-gray-900 dark:text-zinc-200">Carga horária</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody className="divide-y divide-gray-200 dark:divide-zinc-800">
+                                        <tr className="hover:bg-gray-50 dark:hover:bg-zinc-800/30 transition-colors">
+                                            <td className="px-4 py-3 font-medium text-gray-900 dark:text-white">Desenvolvimento Web Full Stack</td>
+                                            <td className="px-4 py-3">10/03/2026</td>
+                                            <td className="px-4 py-3">15/12/2026</td>
+                                            <td className="px-4 py-3 text-right">360h</td>
+                                        </tr>
+
+                                        <tr className="hover:bg-gray-50 dark:hover:bg-zinc-800/30 transition-colors">
+                                            <td className="px-4 py-3 font-medium text-gray-900 dark:text-white">Introdução à Inteligência Artificial</td>
+                                            <td className="px-4 py-3">05/04/2026</td>
+                                            <td className="px-4 py-3">28/06/2026</td>
+                                            <td className="px-4 py-3 text-right">60h</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </section>
                         </main>
                     </div>
                 </div>
             </SidebarProvider>
-        <Footer/>
+            <Footer />
         </>
     )
 }
--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -13,13 +13,38 @@ function Reports() {
                     <div className="flex flex-1 w-full">
                         <AppSidebar />
 
-                        <main className="p-4">
+                        <main className="p-4 flex-1">
                             <h1 className="text-4xl font-bold overflow-hidden">Relatórios</h1>
+                            <section className='mt-4'>
+
+                                <table className="min-w-full divide-y border border-zinc-200 divide-gray-200 bg-white dark:bg-zinc-900 text-sm">
+                                    <thead className="bg-gray-50 dark:bg-zinc-800/50">
+                                        <tr>
+                                            <th className="px-4 py-3 text-left font-semibold text-gray-900 dark:text-zinc-200">Nome completo</th>
+                                            <th className="px-4 py-3 text-left font-semibold text-gray-900 dark:text-zinc-200">Projeto'</th>
+                                            <th className="px-4 py-3 text-left font-semibold text-gray-900 dark:text-zinc-200">Data</th>
+                                            <th className="px-4 py-3 text-right font-semibold text-gray-900 dark:text-zinc-200">Entrada</th>
+                                            <th className="px-4 py-3 text-right font-semibold text-gray-900 dark:text-zinc-200">Saída</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody className="divide-y divide-gray-200 dark:divide-zinc-800">
+                                        <tr className="hover:bg-gray-50 dark:hover:bg-zinc-800/30 transition-colors">
+                                            <td className="px-4 py-3 font-medium text-gray-900 dark:text-white">Ana Oliveira</td>
+                                            <td className="px-4 py-3">InovaEdu</td>
+                                            <td className="px-4 py-3">01/02/2025</td>
+                                            <td className="px-4 py-3 text-right">13:30</td>
+                                            <td className="px-4 py-3 text-right">14:30</td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </section>
                         </main>
                     </div>
                 </div>
             </SidebarProvider>
-            <Footer/>
+            <Footer />
         </>
     )
 }
