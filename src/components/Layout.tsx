560dfb75a3c7e7b763b396293675b3c268bd9930
Eduardoh5179 <eduardorsim7@gmail.com>
2026-06-17 14:22:44 -0300

    feat:adicionando arquivo do commit anterior

---
+++ src/components/Layout.tsx
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+import { SidebarProvider } from "./ui/sidebar";
+import { AppSidebar } from "./app-sidebar";
+import Header from "./Header";
+import Footer from "./Footer";
+import { useAuth } from "../context/AuthContext";
+
+function Layout() {
+  const { role } = useAuth();
+
+  return (
+    <SidebarProvider defaultOpen={role === 'student' || role === 'teacher'}>
+      <div className="flex flex-col w-screen min-h-screen bg-zinc-50/50">
+        <Header />
+        
+        <div className="flex flex-1 w-full items-start">
+          <AppSidebar />
+          
+
+          <main className="flex-1">
+            <Outlet />
+          </main>
+        </div>
+
+        <Footer />
+      </div>
+    </SidebarProvider>
+  );
+}
+
+export default Layout
