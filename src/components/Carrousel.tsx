6c95bd52db8cb63b56a27f3ae751755e90f778b6
Eduardoh5179 <eduardorsim7@gmail.com>
2026-06-24 17:26:26 -0300

    feat:ajustando botões do swiper para funcionarem no mobile e mudando estilos deles

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -10,9 +10,9 @@ function Carrousel() {
 
         <>
             <section >
-                <div className="w-full max-w-7xl overflow-hidden">
+                <div className="w-full max-w-7xl overflow-hidden ">
                     <div className="w-full border border-zinc-400 bg-card mt-2 relative group rounded-md overflow-hidden">
-                    <h1 className='absolute z-10 text-xl md:text-3xl font-semibold top-2 left-3 '>Notícias</h1>
+                        <h1 className='absolute z-10 text-xl md:text-3xl font-semibold top-2 left-3 '>Notícias</h1>
                         <Swiper
                             modules={[Navigation, Autoplay]}
                             navigation={{
@@ -28,11 +28,11 @@ function Carrousel() {
                                 },
                             }}
 
-                            style={{ width: '100%', overflow: 'hidden' }} // força o swiper a respeitar o container
+                            style={{ width: '100%', overflow: 'hidden' }} 
                             className="w-full"
                         >
                             {Array.from({ length: 6 }).map((_, index) => (
-                                <SwiperSlide key={index} style={{ minWidth: 0 }}> {/* evita que o slide expanda */}
+                                <SwiperSlide key={index} style={{ minWidth: 0 }}> 
                                     <div className="flex w-full aspect-video items-center justify-center bg-muted text-foreground border-zinc-400 md:border-r md:last:border-r-0 select-none">
                                         <span className="text-2xl font-semibold">{index + 1}</span>
                                     </div>
@@ -40,13 +40,13 @@ function Carrousel() {
                             ))}
                         </Swiper>
 
-                        <button className="swiper-button-custom-prev hidden md:flex absolute left-4 top-1/2 -translate-y-1/2 z-10 bg-background border border-zinc-300 w-10 h-10 rounded-full items-center justify-center cursor-pointer shadow-md hover:bg-accent transition-colors">
+                        <button className="swiper-button-custom-prev flex absolute left-2 md:left-4 top-1/2 -translate-y-1/2 z-10 bg-background border border-zinc-300 w-8 h-8 md:w-10 md:h-10 rounded-full items-center justify-center cursor-pointer shadow-md hover:bg-neutral-300 transition-colors">
                             <ChevronLeft className="h-4 w-4 text-foreground" />
                         </button>
 
-                        <button className="swiper-button-custom-next hidden md:flex absolute right-4 top-1/2 -translate-y-1/2 z-10 bg-background border border-zinc-300 w-10 h-10 rounded-full items-center justify-center cursor-pointer shadow-md hover:bg-accent transition-colors">
+                        <button className="swiper-button-custom-next flex absolute right-2 md:right-4 top-1/2 -translate-y-1/2 z-10 bg-background border border-zinc-300 w-8 h-8 md:w-10 md:h-10 rounded-full items-center justify-center cursor-pointer shadow-md hover:bg-neutral-300 transition-colors">
                             <ChevronRight className="h-4 w-4 text-foreground" />
-                        </button> 
+                        </button>
                     </div>
                 </div>
             </section>
