7a3fc99be6bd73065b5ac42aad7b57d03839e0dc
Eduardoh5179 <eduardorsim7@gmail.com>
2026-07-02 15:43:27 -0300

    style: pequenos ajustes na tela de presença

--- src/pages/Presença.tsx
+++ src/pages/Presença.tsx
@@ -65,7 +65,7 @@ export default function Presença() {
 
         <section className='flex-1 flex text-center justify-center items-center'>
 
-                <div className="w-full max-w-sm rounded-2xl bg-white p-6 shadow-xl shadow-slate-100">
+                <div className="w-full max-w-sm rounded-2xl bg-white p-6 border border-zinc-300">
 
                     <div className="text-center">
                         <h1 className="text-xl font-bold text-slate-800">Registrar Ponto</h1>
@@ -74,26 +74,25 @@ export default function Presença() {
                         </p>
                     </div>
 
-                    <div className="my-6 rounded-xl border-2 border-slate-100 bg-slate-50/50 p-5 text-center">
-                        <span className="text-xs font-bold uppercase tracking-wider text-slate-400">
-                            Tempo Trabalhado
+                    <div className="my-6 rounded-xl border-2 border-slate-200 bg-slate-50/50 p-5 text-center">
+                        <span className="text-xs font-bold uppercase tracking-wider text-zinc-400">
+                            Tempo Trabalhando no projeto
                         </span>
                         <p className="mt-1 font-mono text-4xl font-bold tracking-tight text-slate-800">
                             {formatarCronometro(segundos)}
                         </p>
                     </div>
 
-                    <button
-                        onClick={alternarPonto}
-                        className={`w-full rounded-xl cursor-pointer py-4 text-base font-bold text-white transition-all duration-200 active:scale-[0.98] ${estaTrabalhando
+                    <button onClick={alternarPonto}
+                        className={`w-full rounded-xl hover:translate-y-[1px] active:translate-y-[3px] cursor-pointer py-4 text-base font-bold text-white transition-all duration-200 ${estaTrabalhando
                                 ? 'bg-rose-500 hover:bg-rose-600 shadow-lg shadow-rose-100'
-                                : 'bg-emerald-500 hover:bg-emerald-600 shadow-lg shadow-emerald-100'
+                                : 'bg-(--lightCyan) hover:bg-(--cyanHover) shadow-sm '
                             }`}
                     >
                         {estaTrabalhando ? 'Registrar Saída' : 'Registrar Entrada'}
                     </button>
 
-                    <div className="mt-6 border-t border-slate-100 pt-4">
+                    <div className="mt-6 pt-4">
                         <h3 className="text-xs font-bold uppercase tracking-wider text-slate-500 mb-3">
                             Registro Atual:
                         </h3>
