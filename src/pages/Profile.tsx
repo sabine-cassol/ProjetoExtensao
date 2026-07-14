5b31167977b3874b542b7ad4a422264fcdeeac03
Eduardoh5179 <eduardorsim7@gmail.com>
2026-07-14 01:04:11 -0300

    feat: adicionando texto informativo

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -186,7 +186,7 @@ function Profile() {
                                         }`}
                                 />
                             </div>)}
-                            
+
                             {role === 'student' && (<div>
                                 <label htmlFor="curso" className="font-segoe text-sm font-medium text-zinc-700 flex items-center gap-2"> Curso </label>
                                 <input id="curso" name="curso" type="text" value={formData.curso} onChange={handleChange} readOnly={!isEditing} className={`border w-full mt-2 text-zinc-800 border-zinc-400 font-normal rounded-md p-2 focus:outline-none transition-all
@@ -222,7 +222,10 @@ function Profile() {
                             )}
                         </div>
                         <div className="flex flex-col p-4 gap-1.5">
-                            <div className="font-segoe text-lg font-bold text-zinc-900 mb-2">Alterar senha</div>
+                            <div>
+                                <div className="font-segoe text-lg font-bold text-zinc-900 ">Alterar senha</div>
+                                <p className="text-[#9198a1] text-xs"> Caso queira definir uma nova senha de acesso (opcional) </p>
+                            </div>
                             <form onSubmit={handleSubmit}>
                                 <div className="relative">
                                     <label htmlFor="password" className="font-segoe text-sm font-medium text-zinc-700 flex items-center gap-2">Digite a senha nova</label>
