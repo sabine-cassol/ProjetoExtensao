39564019142cfd74d98a91e7940da0dbcd3970b9
Eduardoh5179 <eduardorsim7@gmail.com>
2026-07-15 16:28:59 -0300

    feat: mudando permissões para editar projeto para apenas o professor responsável

--- backend/services/projetoService.js
+++ backend/services/projetoService.js
@@ -13,28 +13,38 @@ export default (projetoRepository) => {
         async listarTodos() {
             return projetoRepository.listarTodos();
         },
-        async listarTodosPorProfessor(professorId) { 
+        async listarTodosPorProfessor(professorId) {
             return projetoRepository.listarTodosPorProfessor(professorId);
         },
-        async atualizarProjeto(id, dados) {
+        async atualizarProjeto(id, dados, professorLogadoId) {
             const projeto = await projetoRepository.atualizarProjeto(id, dados);
             if (!projeto) {
                 throw new Error("Projeto de extensão não encontrado");
             }
-            return projeto; 
+            if (projeto.professorId !== professorLogadoId) {
+                throw new Error("Você não tem permissão para editar este projeto");
+            }
+            const projetoAtualizado = await projetoRepository.atualizarProjeto(id, dados);
+            return projetoAtualizado;
         },
-        async desativarProjeto(id) {
-            const projetoDesativado = await projetoRepository.buscarPorId(id);
-            if (!projetoDesativado) {
+        async desativarProjeto(id, professorLogadoId) {
+            const projeto = await projetoRepository.buscarPorId(id);
+            if (!projeto) {
                 throw new Error("Projeto de extensão não encontrado");
             }
-            projetoDesativado.ativo = false;
-            return projetoRepository.atualizarProjeto(id, projetoDesativado);
+            if (projeto.professorId !== professorLogadoId) {
+                throw new Error("Você não tem permissão para desativar este projeto");
+            }
+            projeto.ativo = false;
+            return projetoRepository.atualizarProjeto(id, projeto);
         },
         async ativarProjeto(id) {
             const projetoAtivado = await projetoRepository.buscarPorId(id);
             if (!projetoAtivado) {
                 throw new Error("Projeto de extensão não encontrado");
+            }
+            if (projeto.professorId !== professorLogadoId) {
+                throw new Error("Você não tem permissão para desativar este projeto");
             }
             projetoAtivado.ativo = true;
             return projetoRepository.atualizarProjeto(id, projetoAtivado);
